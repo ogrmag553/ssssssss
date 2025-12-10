52f6b234a8f6f67ce2f5357bb7a534e7290b9986
ogrmag553 <ogrmag553@gmail.com>
2025-12-10 18:55:57 +0500

    12
    132

--- backend/Users/admin.py
+++ backend/Users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category,Tag,Post,Comment
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin)
+    list_display =['name','description']
+
+@admin.register(Tag)   
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name'] 
+
+@admin.register(Post) 
+class PostAdmin(admin.ModelAdmin):
+    list_display =['titell','autor','category','create_ar'] 
+    list_filter = ['tag','create_ar','category']  
+    list_fields = ['titell','comment']
+    
--- backend/Users/models.py
+++ backend/Users/models.py
@@ -9,3 +9,27 @@ class User(models.Model):
     def __str__(self):
         return self.name 
 
+
+
+
+class Autor(models.Model):
+    name = models.CharField(max_length=50)
+    biography = models. TimeField()
+    birth_day = models.DateField(null=True)
+
+    def __str__(self) -> str
+       return self.name
+    
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    published_date = models.DateField()
+    isbh = models.CharField(max_length=12,unique=True)
+
+
+Autor = models.ManyToManyField(Autor)
+
+def __str__(self) -> str:
+    return self.titele
+
+
+
